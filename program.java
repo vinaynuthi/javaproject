03387b12935d76b749892614ddd2f31f408e40b5
vinaynuthi <vinaynuthi2001@gmail.com>
2024-03-05 22:16:12 +0530

    java program

---
+++ program.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        scanner.close();
+    }
+}
+
