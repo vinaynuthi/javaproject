11b27f5a276b2f1101611aded8e38cf8c9e2cabd
vinaynuthi <159252942+vinaynuthi@users.noreply.github.com>
2024-03-12 14:44:25 +0530

    Update program.java

--- program.java
+++ program.java
@@ -1,20 +1,9 @@
-import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the first number:");
-        double num1 = scanner.nextDouble();
-
-        System.out.println("Enter the second number:");
-        double num2 = scanner.nextDouble();
-
-        double sum = num1 + num2;
-
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-
-        scanner.close();
+        int a = 10;
+        int b = 11;
+        int c = a+b;
     }
 }
 
